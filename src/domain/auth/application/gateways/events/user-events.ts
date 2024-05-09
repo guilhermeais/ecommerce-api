4a2318c2fc41a2d085eac7fed97793c447be3dbc
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-09 08:42:02 -0300

    feat: add UserEvents class for publishing user-related events

---
+++ src/domain/auth/application/gateways/events/user-events.ts
@@ -0,0 +1,13 @@
+import { Event } from '@/core/types/event';
+import { User } from '@/domain/auth/enterprise/entities/user';
+
+export type UserEventsMap = {
+  'user.created': User;
+};
+
+export abstract class UserEvents implements Event<UserEventsMap> {
+  abstract publish<K extends keyof UserEventsMap>(
+    event: K,
+    data: UserEventsMap[K],
+  ): Promise<void>;
+}
