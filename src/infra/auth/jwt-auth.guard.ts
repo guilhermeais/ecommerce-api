75b886ed1080af5e44665df05164de6369bdab29
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-09 21:29:51 -0300

    feat: Update JWTAuthGuard to handle public routes and user roles

--- src/infra/auth/jwt-auth.guard.ts
+++ src/infra/auth/jwt-auth.guard.ts
@@ -1,6 +1,10 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { ROLES_KEY } from './roles.decorator';
+import { Role } from '@/domain/auth/enterprise/entities/enums/role';
+import { IS_PUBLIC_KEY } from './public.decorator';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -9,12 +13,29 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   }
 
   canActivate(context: ExecutionContext) {
-    const roles = this.reflector.getAllAndOverride<string[]>('ROLES', [
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    console.log(roles);
+    if (isPublic) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest<Request>();
+
+    if (!user) {
+      return false;
+    }
+
+    const roles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (roles && !roles.includes(user.role)) {
+      return false;
+    }
 
     return super.canActivate(context);
   }
