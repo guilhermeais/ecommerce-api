bf23d88a7d37dbf1df4f35c1bd67d6f8227df007
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-11 10:11:52 -0300

    refactor: Update Encrypter interface to include optional encryption options

--- src/domain/auth/application/gateways/cryptography/encrypter.ts
+++ src/domain/auth/application/gateways/cryptography/encrypter.ts
@@ -1,4 +1,11 @@
+export type EncryptOptions = {
+  expiresIn: number;
+};
+
 export abstract class Encrypter {
-  abstract encrypt(payload: Record<string, unknown>): Promise<string>;
+  abstract encrypt(
+    payload: Record<string, unknown>,
+    options?: EncryptOptions,
+  ): Promise<string>;
   abstract decode(token: string): Promise<Record<string, unknown>>;
 }
