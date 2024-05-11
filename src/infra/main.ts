1717d5cf58c97905510ea3cd7dd04bc2c412c429
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-11 09:27:05 -0300

    feat: Add global exception filter for handling HTTP request exceptions

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
+import { DefaultExceptionFilter } from './http/filters/default-exception-filter.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new DefaultExceptionFilter());
   const configService = app.get(EnvService);
   const port = configService.get('PORT');
 
