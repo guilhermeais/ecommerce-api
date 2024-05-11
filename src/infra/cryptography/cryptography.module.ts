328f323a97dfcb9fe145f4588ab9a5b782171b78
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-11 09:27:20 -0300

    feat: Add EnvModule to CryptographyModule imports

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -3,8 +3,10 @@ import { Hasher } from '@/domain/auth/application/gateways/cryptography/hasher';
 import { Module } from '@nestjs/common';
 import { BcryptHasher } from './bcrpyt-hahser';
 import { JwtEncrypter } from './jwt-encrypter';
+import { EnvModule } from '../env/env.module';
 
 @Module({
+  imports: [EnvModule],
   providers: [
     { provide: Hasher, useClass: BcryptHasher },
     {
