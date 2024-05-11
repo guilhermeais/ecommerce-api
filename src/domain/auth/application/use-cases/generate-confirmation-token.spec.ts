66fbd2f299c874a348d6bf78be5e41e04a56caca
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-11 10:12:43 -0300

    feat: create GenerateConfirmationTokenUseCase for generating confirmation tokens

---
+++ src/domain/auth/application/use-cases/generate-confirmation-token.spec.ts
@@ -0,0 +1,105 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { EntityNotFoundError } from '@/core/errors/commom/entity-not-found-error';
+import { InMemoryConfirmationTokensRepository } from '@/infra/database/in-memory/repositories/in-memory-confirmation-tokens.repository';
+import { InMemoryUserRepository } from '@/infra/database/in-memory/repositories/in-memory-user-repository';
+import { Logger } from '@/shared/logger';
+import { faker } from '@faker-js/faker';
+import { FakeEncrypter } from 'test/auth/application/gateways/cryptography/fake-encrypter';
+import { makeUser } from 'test/auth/enterprise/entities/make-user';
+import { makeFakeLogger } from 'test/shared/logger.mock';
+import { Encrypter } from '../gateways/cryptography/encrypter';
+import { ConfirmationTokensRepository } from '../gateways/repositories/confirmation-tokens-repository';
+import { UserRepository } from '../gateways/repositories/user-repository';
+import {
+  GenerateConfirmationTokenRequest,
+  GenerateConfirmationTokenUseCase,
+} from './generate-confirmation-token';
+
+describe('GenerateConfirmationTokenUseCase', () => {
+  let sut: GenerateConfirmationTokenUseCase;
+  let userRepository: UserRepository;
+  let confirmationsTokenRepository: ConfirmationTokensRepository;
+  let logger: Logger;
+  let encrypter: Encrypter;
+
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+
+  beforeEach(() => {
+    userRepository = new InMemoryUserRepository();
+    confirmationsTokenRepository = new InMemoryConfirmationTokensRepository();
+    logger = makeFakeLogger();
+    encrypter = new FakeEncrypter();
+
+    sut = new GenerateConfirmationTokenUseCase(
+      userRepository,
+      logger,
+      encrypter,
+      confirmationsTokenRepository,
+    );
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  function makeRequest(
+    modifications?: Partial<GenerateConfirmationTokenRequest>,
+  ): GenerateConfirmationTokenRequest {
+    return {
+      userId: faker.string.uuid(),
+      ...modifications,
+    };
+  }
+
+  it('should throw EntityNotFoundError if user does not exists', async () => {
+    const request = makeRequest();
+
+    await expect(sut.execute(request)).rejects.toThrowError(
+      new EntityNotFoundError('Usuário', request.userId),
+    );
+  });
+
+  it('should generate a confirmation token to the user', async () => {
+    const existingUserId = new UniqueEntityID();
+    const request = makeRequest({
+      userId: existingUserId.toString(),
+    });
+
+    const existingUser = makeUser({ id: existingUserId });
+    await userRepository.save(existingUser);
+
+    const response = await sut.execute(request);
+
+    expect(response.confirmationId).toBeTypeOf('string');
+    expect(response.token).toBeDefined();
+    expect(response.expiresIn).toBeDefined();
+    expect(response.expiresIn).toBe(1000 * 60 * 60 * 24);
+
+    const decodedToken = await encrypter.decode(response.token);
+
+    expect(decodedToken.userId).toBe(existingUserId.toString());
+    expect(decodedToken.email).toBe(existingUser.email.value);
+    expect(decodedToken.createdAt).toBe(new Date().toISOString());
+
+    expect(
+      await confirmationsTokenRepository.findByToken(response.token),
+    ).toBeDefined();
+  });
+
+  it('should generate a confirmation token with different expiration', async () => {
+    const existingUserId = new UniqueEntityID();
+    const request = makeRequest({
+      userId: existingUserId.toString(),
+      expiresIn: 1000 * 60 * 60 * 24 * 2,
+    });
+
+    const existingUser = makeUser({ id: existingUserId });
+    await userRepository.save(existingUser);
+
+    const response = await sut.execute(request);
+
+    expect(response.expiresIn).toBe(1000 * 60 * 60 * 24 * 2);
+  });
+});
---
+++ src/domain/auth/application/use-cases/generate-confirmation-token.ts
@@ -0,0 +1,103 @@
+import { UseCase } from '@/core/types/use-case';
+import { UserRepository } from '../gateways/repositories/user-repository';
+import { Logger } from '@/shared/logger';
+import { Encrypter } from '../gateways/cryptography/encrypter';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { EntityNotFoundError } from '@/core/errors/commom/entity-not-found-error';
+import { ConfirmationToken } from '../../enterprise/entities/confirmation-token';
+import { ConfirmationTokensRepository } from '../gateways/repositories/confirmation-tokens-repository';
+
+export type GenerateConfirmationTokenRequest = {
+  userId: string;
+  expiresIn?: number;
+};
+
+export type GenerateConfirmationTokenResponse = {
+  confirmationId: string;
+  token: string;
+  expiresIn: number;
+};
+
+export class GenerateConfirmationTokenUseCase
+  implements
+    UseCase<
+      GenerateConfirmationTokenRequest,
+      GenerateConfirmationTokenResponse
+    >
+{
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly logger: Logger,
+    private readonly encrypter: Encrypter,
+    private readonly confirmationTokenRepository: ConfirmationTokensRepository,
+  ) {}
+
+  async execute(
+    request: GenerateConfirmationTokenRequest,
+  ): Promise<GenerateConfirmationTokenResponse> {
+    try {
+      this.logger.log(
+        GenerateConfirmationTokenUseCase.name,
+        `Generating confirmation token for user ${request.userId}`,
+      );
+
+      const userId = new UniqueEntityID(request.userId);
+
+      const user = await this.userRepository.findById(userId);
+
+      if (!user) {
+        throw new EntityNotFoundError('Usuário', request.userId);
+      }
+      const ONE_DAY_MS = 1000 * 60 * 60 * 24;
+
+      const expiresIn = request.expiresIn || ONE_DAY_MS;
+
+      const payload = {
+        userId: user.id.toString(),
+        email: user.email.value,
+        createdAt: new Date().toISOString(),
+      };
+
+      this.logger.log(
+        GenerateConfirmationTokenUseCase.name,
+        `Generating confirmation token for user ${request.userId} with expiration ${expiresIn}: ${JSON.stringify(payload)}`,
+      );
+
+      const token = await this.encrypter.encrypt(payload, {
+        expiresIn,
+      });
+
+      this.logger.log(
+        GenerateConfirmationTokenUseCase.name,
+        `Generated confirmation token for user ${request.userId}: ${token}`,
+      );
+
+      const confirmationToken = ConfirmationToken.create({
+        token,
+        expiresIn,
+        userId: user.id.toString(),
+      });
+
+      this.logger.log(
+        GenerateConfirmationTokenUseCase.name,
+        `Saving confirmation token for user ${request.userId}`,
+      );
+
+      await this.confirmationTokenRepository.save(confirmationToken);
+
+      return {
+        token,
+        expiresIn,
+        confirmationId: confirmationToken.id.toString(),
+      };
+    } catch (error: any) {
+      this.logger.error(
+        GenerateConfirmationTokenUseCase.name,
+        `Error generating confirmation token for user ${request.userId}`,
+        error.stack,
+      );
+
+      throw error;
+    }
+  }
+}
