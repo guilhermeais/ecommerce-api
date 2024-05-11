34734fe4ee4c12c128e83b8940c9c5fcf1ae73d6
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-11 09:27:41 -0300

    feat: Add HttpModule for handling HTTP requests

---
+++ src/infra/http/controllers/http.module.ts
@@ -0,0 +1,13 @@
+import { ClientSignUpUseCase } from '@/domain/auth/application/use-cases/client-sign-up';
+import { CryptographyModule } from '@/infra/cryptography/cryptography.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { EventsModule } from '@/infra/events/events.module';
+import { Module } from '@nestjs/common';
+import { ClientSignUpController } from './auth/client-sign-up.controller';
+
+@Module({
+  imports: [EventsModule, DatabaseModule, CryptographyModule],
+  controllers: [ClientSignUpController],
+  providers: [ClientSignUpUseCase],
+})
+export class HttpModule {}
