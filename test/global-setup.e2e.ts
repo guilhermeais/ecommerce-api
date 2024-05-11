632b8b1231a9a3fa6596ce9373085c44fcb72339
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-11 09:04:08 -0300

    feat: generate RSA key pair for JWT encryption

--- test/global-setup.e2e.ts
+++ test/global-setup.e2e.ts
@@ -1,6 +1,16 @@
+import forge from 'node-forge';
+
 export async function setup() {
-  process.env.JWT_PUBLIC_KEY = 'jwt-public';
-  process.env.JWT_PRIVATE_KEY = 'jwt-private';
+  const { publicKey, privateKey } = forge.pki.rsa.generateKeyPair({
+    bits: 2048,
+    e: 0x10001,
+  });
+
+  const privateKeyPem = forge.pki.privateKeyToPem(privateKey);
+  const publicKeyPem = forge.pki.publicKeyToPem(publicKey);
+
+  process.env.JWT_PRIVATE_KEY = Buffer.from(privateKeyPem).toString('base64');
+  process.env.JWT_PUBLIC_KEY = Buffer.from(publicKeyPem).toString('base64');
 }
 
 export async function teardown() {
