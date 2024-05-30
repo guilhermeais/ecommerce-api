5e342dbf6863a88b153b4e6f3efc4da5830f858f
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-05-29 22:09:29 -0300

    feat: create route to create sub-category

--- src/infra/http/controllers/products/create-category.controller.e2e-spec.ts
+++ src/infra/http/controllers/products/create-category.controller.e2e-spec.ts
@@ -12,17 +12,21 @@ import { UserFactory } from 'test/auth/enterprise/entities/make-user';
 import { makeTestingApp } from 'test/make-testing-app';
 import { CreateCategoryBody } from './create-category.controller';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { CategoryFactory } from 'test/products/enterprise/entities/make-category';
+import { CategoryHTTPResponse } from './presenters/category-presenter';
+import { EntityNotFoundError } from '@/core/errors/commom/entity-not-found-error';
 
 describe('CreateCategoryController (E2E)', () => {
   let app: INestApplication;
   let categoriesRepository: CategoriesRepository;
   let eventManager: EventManager;
   let userFactory: UserFactory;
+  let categoryFactory: CategoryFactory;
 
   beforeAll(async () => {
     const moduleRef = await makeTestingApp({
       imports: [DatabaseModule, CryptographyModule],
-      providers: [UserFactory],
+      providers: [UserFactory, CategoryFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
@@ -32,6 +36,7 @@ describe('CreateCategoryController (E2E)', () => {
     categoriesRepository = moduleRef.get(CategoriesRepository);
     eventManager = moduleRef.get(EventManager);
     userFactory = moduleRef.get(UserFactory);
+    categoryFactory = moduleRef.get(CategoryFactory);
 
     await app.init();
   });
@@ -76,6 +81,69 @@ describe('CreateCategoryController (E2E)', () => {
       );
 
       expect(categoryOnDb).toBeDefined();
+    });
+  });
+
+  describe('[POST] /admin/categories/:rootCategoryId/sub-category', () => {
+    it('should create a sub-category', async () => {
+      const { accessToken } = await userFactory.makeUser({
+        role: Role.ADMIN,
+      });
+
+      const { category: rootCategory } = await categoryFactory.makeCategory();
+
+      const body = makeCreateCategoryBodyRequest();
+
+      const response = await request(app.getHttpServer())
+        .post(`/admin/categories/${rootCategory.id.toString()}/sub-category`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(body);
+
+      expect(response.status).toBe(201);
+      expect(response.body).toMatchObject<CategoryHTTPResponse>({
+        id: expect.any(String),
+        name: body.name,
+        description: body.description,
+        rootCategory: {
+          id: rootCategory.id.toString(),
+          name: rootCategory.name,
+          description: rootCategory.description,
+        },
+      });
+
+      const categoryOnDb = await categoriesRepository.findById(
+        new UniqueEntityID(response.body.id),
+      );
+
+      expect(categoryOnDb).toBeDefined();
+    });
+
+    it('should return 404 if the roo category of an sub-category does not exists', async () => {
+      const { accessToken } = await userFactory.makeUser({
+        role: Role.ADMIN,
+      });
+
+      const body = makeCreateCategoryBodyRequest();
+      const rootCategoryId = faker.string.uuid();
+      const response = await request(app.getHttpServer())
+        .post(`/admin/categories/${rootCategoryId}/sub-category`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(body);
+
+      expect(response.status).toBe(404);
+      const expectedError = new EntityNotFoundError(
+        'Categoria Pai',
+        rootCategoryId,
+      );
+      expect(response.body.error).toEqual(expectedError.name);
+      expect(response.body.message).toEqual([expectedError.message]);
+      expect(response.body.details).toEqual(expectedError.details);
+
+      const categoryOnDb = await categoriesRepository.findById(
+        new UniqueEntityID(response.body.id),
+      );
+
+      expect(categoryOnDb).toBeNull();
     });
   });
 });
--- src/infra/http/controllers/products/create-category.controller.ts
+++ src/infra/http/controllers/products/create-category.controller.ts
@@ -4,7 +4,7 @@ import { CreateCategoryUseCase } from '@/domain/product/application/use-cases/cr
 import { CurrentUser } from '@/infra/auth/current-user.decorator';
 import { Roles } from '@/infra/auth/roles.decorator';
 import { Logger } from '@/shared/logger';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { z } from 'zod';
 import { ZodValidationPipe } from '../../pipes/zod-validation.pipe';
 import {
@@ -36,7 +36,7 @@ export class CreateCategoryController {
 
   @Roles(Role.MASTER, Role.ADMIN)
   @Post()
-  async handle(
+  async createCategory(
     @CurrentUser() currentUser: User,
     @Body(new ZodValidationPipe(CreateCategoryBodySchema))
     body: CreateCategoryBody,
@@ -59,6 +59,42 @@ export class CreateCategoryController {
       this.logger.error(
         CreateCategoryController.name,
         `Error creating category with: ${JSON.stringify(body)}: ${error.message}`,
+        error.stack,
+      );
+
+      throw error;
+    }
+  }
+
+  @Roles(Role.MASTER, Role.ADMIN)
+  @Post(':rootCategoryId/sub-category')
+  async createSubCategory(
+    @CurrentUser() currentUser: User,
+    @Body(new ZodValidationPipe(CreateCategoryBodySchema))
+    body: CreateCategoryBody,
+    @Param('rootCategoryId') rootCategoryId: string,
+  ): Promise<CreateCategoryResponse> {
+    try {
+      this.logger.log(
+        CreateCategoryController.name,
+        `User ${currentUser.id.toString()} - ${currentUser.name} Creating sub-category with: ${JSON.stringify(body)}.`,
+      );
+
+      const result = await this.createCategoryUseCase.execute({
+        ...body,
+        rootCategoryId,
+      });
+
+      this.logger.log(
+        CreateCategoryController.name,
+        `User ${currentUser.id.toString()} - ${currentUser.name} Created sub-category with: ${JSON.stringify(body)}`,
+      );
+
+      return CategoryPresenter.toHTTP(result);
+    } catch (error: any) {
+      this.logger.error(
+        CreateCategoryController.name,
+        `Error creating sub-category with: ${JSON.stringify(body)}: ${error.message}`,
         error.stack,
       );
 
