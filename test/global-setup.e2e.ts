5c528d0b47ddf9633fa51d107a08064206b383f8
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-06-07 12:44:55 -0300

    refactor: Update Google Cloud Run secrets and environment variables

--- test/global-setup.e2e.ts
+++ test/global-setup.e2e.ts
@@ -1,9 +1,9 @@
-import forge from 'node-forge';
 import { Env } from '@/infra/env/env';
 import {
   MongoDBContainer,
   StartedMongoDBContainer,
 } from '@testcontainers/mongodb';
+import forge from 'node-forge';
 
 let mongoContainer: StartedMongoDBContainer;
 
@@ -42,11 +42,7 @@ export async function setup() {
     JWT_PUBLIC_KEY: Buffer.from(publicKeyPem).toString('base64'),
     APP_NAME: 'PiaLabs Ecommerce',
     APP_EMAIL: 'test@mail.com',
-    GOOGLE_CLIENT_ID: 'fake-client-id',
-    GOOGLE_CLIENT_SECRET: 'fake-secret',
-    GOOGLE_REFRESH_TOKEN: 'fake-refresh-token',
     ACCOUNT_CONFIRMATION_URL: 'http://localhost:3000/confirm',
-    GOOGLE_REDIRECT_URI: 'https://developers.google.com/oauthplayground',
 
     CONFIRMATION_TOKEN_EXPIRES_IN: 1000 * 60 * 60 * 24,
     FINISH_SIGNUP_INVITE_URL: 'http://localhost:3000/finish-signup-invite',
@@ -58,6 +54,9 @@ export async function setup() {
     GOOGLE_APPLICATION_CREDENTIALS:
       Buffer.from('fake-credentials').toString('base64'),
     GOOGLE_STORAGE_BUCKET: 'fake-bucket',
+    GOOGLE_GMAIL_PASSWORD: 'fake-password',
+    GOOGLE_GMAIL_USER: 'fake-user',
+    JWT_EXPIRES_IN: 1000 * 60 * 60 * 24,
   };
 
   Object.entries(mockedEnvs).forEach(([key, value]) => {
