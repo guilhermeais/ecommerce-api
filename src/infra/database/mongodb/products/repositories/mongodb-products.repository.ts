d7e32d3ef261f1853131fd359671573540826762
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-06-15 08:52:08 -0300

    chore: Update MongoDB product repository to use 'id' instead of '_id'

--- src/infra/database/mongodb/products/repositories/mongodb-products.repository.ts
+++ src/infra/database/mongodb/products/repositories/mongodb-products.repository.ts
@@ -29,7 +29,7 @@ export class MongoDbProductsRepository implements ProductsRepository {
       );
 
       const exists = await this.productModel.exists({
-        _id: product.id.toString(),
+        id: product.id.toString(),
       });
 
       const productModel = MongoDbProductMapper.toPersistence(product);
